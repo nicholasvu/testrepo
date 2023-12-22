2b3146e0af4ccd2397bbf6ae00af29a3cbb101a4
Nicholas Vu <nicholasvu@Nicholass-MacBook-Pro.local>
2023-12-22 18:07:57 -0500

    Updating Code

---
+++ Week 03/java1operations.js
@@ -0,0 +1,22 @@
+//Swap variable
+
+import java.io.*;
+import java.util.*;
+
+public class SwapStorage {
+
+      public static void swapNumbers(int x, int y) {
+int temporary = x;
+x = y;
+y = temporary;
+System.out.println("After swapping, x is equal to " + x + " and y is equal to " + y + ".");
+}
+  public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+  int a,b;
+  a = in.nextInt();
+  b = in.nextInt();
+  swapNumbers(a, b);
+}
+
+//Nadalisthe greatest
